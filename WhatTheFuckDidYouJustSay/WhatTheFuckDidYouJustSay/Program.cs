3ab49b27364e50d49fa0f6160469851bfb5053bb
CptJesusH <lmichael1221@gmail.com>
2017-06-07 23:06:52 -0500

    I spent way too much time making my computer say racial slurs.

---
+++ WhatTheFuckDidYouJustSay/WhatTheFuckDidYouJustSay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace WhatTheFuckDidYouJustSay {
+    class Program {
+        static void Main(string[] args) {
+
+            SpeechSynthesizer speech = new SpeechSynthesizer();
+            while (true) {
+                Console.WriteLine("Tell me what to say!");
+                string whatToSay = Console.ReadLine();
+                speech.Speak(whatToSay);
+                continue;
+            }
+            
+
+        }
+    }
+}
